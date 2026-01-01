2e29610c3c899dac9d5a2bf01891df0a05ad248b
Naresh Gantala <nareshgantala@gmail.com>
2026-01-01 18:38:03 +0530

    removed hard coded keys

--- variables.tf
+++ variables.tf
@@ -1,10 +1,8 @@
 variable "key_name" {
-  default = "jiradc"
   description = "EC2 key pair name"
 }
 
 variable "db_password" {
   description = "RDS password"
-  default = "admin1234"
   sensitive   = true
 }
